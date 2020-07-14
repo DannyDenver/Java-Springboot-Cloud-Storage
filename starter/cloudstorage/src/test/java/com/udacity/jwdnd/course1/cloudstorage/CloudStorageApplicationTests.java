7c698514facf9ebecde37493cdc264fa7bf92794
DannyDenver <djt09d@fulbrightmail.org>
2020-07-14 11:55:31 -0600

    add selenium tests for credentials and notes

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -6,9 +6,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
@@ -30,6 +36,8 @@ class CloudStorageApplicationTests {
 	{
 		baseUrl = baseUrl = "http://localhost:" + this.port;
 		this.driver = new ChromeDriver();
+		this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
 		String first = "Dan";
 		String last = "Tay";
 		String username = "dan";
@@ -77,32 +85,161 @@ class CloudStorageApplicationTests {
 	}
 
 	@Test
-	public void createNoteVerifyExists() {
+	public void createEditDeleteNote() throws InterruptedException {
 		String title = "test";
 		String description = "description of note";
 
 		HomePage homePage = new HomePage(driver);
 
-		WebDriverWait wait = new WebDriverWait(driver, 1000);
-		WebElement marker = wait.until(webDriver -> webDriver.findElement(By.id("nav-notes-tab")));
+		WebDriverWait wait = new WebDriverWait(driver, 4, 500);
+		WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='#nav-notes']")));
 
-		homePage.navigateToNotesTab();
+		WebElement tab = driver.findElement(By.xpath("//a[@href='#nav-notes']"));
 
-		WebElement marker2 = wait.until(webDriver -> webDriver.findElement(By.id("showNoteModalButton")));
+		tab.click();
+		Thread.sleep(500);
 
-		homePage.showNoteModal();
+		WebDriverWait wait2 = new WebDriverWait(driver, 10, 500);
+		WebElement button2 = wait2.until(ExpectedConditions.elementToBeClickable(By.id("showNoteModalButton")));
 
-		WebElement marker3= wait.until(webDriver -> webDriver.findElement(By.id("noteModal")));
+		homePage.showNotesModal();
+		WebDriverWait wait3 = new WebDriverWait(driver, 4, 2000);
+		wait2.until(webDriver -> webDriver.findElement(By.id("noteModal")));
 
 		homePage.createNote(title, description);
+
+		WebDriverWait wait4 = new WebDriverWait(driver, 4);
+		wait3.until(webDriver -> webDriver.findElement(By.id("results-view")));
+
+		Assertions.assertEquals("Result", driver.getTitle());
+
+		driver.get(baseUrl + "/home");
+		WebDriverWait wait5 = new WebDriverWait(driver, 4);
+		wait4.until(webDriver -> webDriver.findElement(By.className("noteTitle")));
+
+		homePage = new HomePage(driver);
+
+		WebElement tab2 = driver.findElement(By.xpath("//a[@href='#nav-notes']"));
+
+		tab2.click();
+
+		WebElement note = new WebDriverWait(driver, 2).until(driver -> driver.findElement(By.className("noteTitle")));
+
+		Thread.sleep(2000);
+
+		Assertions.assertEquals(title, homePage.geFirstNoteTitle());
+		Assertions.assertEquals(description, homePage.getFirstNoteDescription());
+
+		String editedTitle = "test2";
+		String editedDescription = "description of note edit";
+
+		homePage.editNote(editedTitle, editedDescription);
+
+		WebDriverWait wait6 = new WebDriverWait(driver, 4, 500);
+		wait6.until(webDriver -> webDriver.findElement(By.id("results-view")));
+
+		Assertions.assertEquals("Result", driver.getTitle());
+		Thread.sleep(5000);
+
 		driver.get(baseUrl + "/home");
 
 		homePage = new HomePage(driver);
 
-		Assertions.assertEquals(title, homePage.geFirstNoteTitle());
-		Assertions.assertEquals(description, homePage.getFirstNoteDescription());
+		homePage.showNotes();
+
+		WebElement tab3 = driver.findElement(By.xpath("//a[@href='#nav-notes']"));
+
+		tab3.click();
+
+		WebElement note2 = new WebDriverWait(driver, 2).until(driver -> driver.findElement(By.className("noteTitle")));
+
+		Thread.sleep(2000);//
+
+		Assertions.assertEquals(editedTitle, homePage.geFirstNoteTitle());
+		Assertions.assertEquals(editedDescription, homePage.getFirstNoteDescription());
+		Thread.sleep(2000);
+
+		homePage.deleteNote();
+
+		driver.get(baseUrl + "/home");
+		Thread.sleep(2000);
+
+		homePage = new HomePage(driver);
+
+		HomePage finalHomePage = homePage;
+		Assertions.assertThrows(org.openqa.selenium.NoSuchElementException.class, () -> finalHomePage.getFirstNoteDescription());
 	}
 
+	@Test
+	public void createViewEditDeleteCredential() throws InterruptedException {
+		String url = "google.com";
+		String username = "dan";
+		String password = "isawesome44";
 
+		HomePage homePage = new HomePage(driver);
 
+		WebDriverWait wait = new WebDriverWait(driver, 4, 500);
+		WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='#nav-credentials']")));
+
+		button.click();
+		Thread.sleep(500);
+
+		homePage.showNewCredentialModal();
+		Thread.sleep(2000);
+
+		homePage.addCredential(url, username, password);
+
+		Thread.sleep(2000);
+		Assertions.assertEquals("Result", driver.getTitle());
+
+		driver.get(baseUrl + "/home");
+
+		HomePage homePage2 = new HomePage(driver);
+
+		WebDriverWait wait2 = new WebDriverWait(driver, 4, 500);
+		WebElement navCredentialsButton2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='#nav-credentials']")));
+
+		navCredentialsButton2.click();
+
+		Thread.sleep(1000);
+
+		Assertions.assertEquals(homePage2.getFirstCredentialUsername(), username);
+		Assertions.assertEquals(homePage2.getFirstCredentialUrl(), url);
+		Assertions.assertNotEquals(homePage2.getFirstCredentialEncryptedPassword(), password);
+
+		homePage2.showCredentialModal();
+		Thread.sleep(1000);
+
+		Assertions.assertEquals(homePage2.getUnencryptedPassword(), password);
+		Thread.sleep(2000);
+
+		String newUsername = "dantheman";
+
+		homePage2.editCredentialUsername(newUsername);
+
+		Thread.sleep(500);
+		Assertions.assertEquals("Result", driver.getTitle());
+
+		driver.get(baseUrl + "/home");
+
+		HomePage homePage3 = new HomePage(driver);
+
+		WebDriverWait wait3 = new WebDriverWait(driver, 4, 500);
+		WebElement navCredentialsButton3 = wait3.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='#nav-credentials']")));
+
+		navCredentialsButton3.click();
+
+		Thread.sleep(1000);
+
+		Assertions.assertEquals(homePage3.getFirstCredentialUsername(), newUsername);
+
+		homePage3.deleteCredential();
+
+		Thread.sleep(500);
+		Assertions.assertEquals("Result", driver.getTitle());
+
+		driver.get(baseUrl + "/home");
+
+		Assertions.assertThrows(org.openqa.selenium.NoSuchElementException.class, () -> homePage3.getFirstCredentialUrl());
+	}
 }
--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,7 +18,10 @@ public class HomePage {
     @FindBy(id="nav-credentials-tab")
     private WebElement credentialsTab;
 
-    @FindBy(id="showNoteModalButton")
+    @FindBy(xpath="//a[@href='#nav-notes']")
+    private WebElement showNotesLink;
+
+    @FindBy(id = "showNoteModalButton")
     private WebElement showNoteModalButton;
 
     @FindBy(id="note-title")
@@ -35,7 +39,46 @@ public class HomePage {
     @FindBy(className="noteDescription")
     private WebElement firstNoteDescription;
 
+    @FindBy(className = "edit-note")
+    private WebElement editNoteButton;
+
+    @FindBy(className = "delete-note")
+    private WebElement deleteNoteButton;
+
+    @FindBy(id="showCredentialModal")
+    private WebElement showCredentialModalButton;
+
+    @FindBy(id="credential-url")
+    private WebElement credentialUrlInput;
+
+    @FindBy(id="credential-username")
+    private WebElement credentialUsernameInput;
+
+    @FindBy(id="credential-password")
+    private WebElement credentialPasswordInput;
+
+    @FindBy(id="credentialSubmit")
+    private WebElement submitCredentialButton;
+
+    @FindBy(className = "credential-url")
+    private WebElement firstCredentialUrl;
+
+    @FindBy(className = "credential-username")
+    private WebElement firstCredentialUsername;
+
+    @FindBy(className = "credential-password")
+    private WebElement firstCredentialEncryptedPassword;
+
+    @FindBy(className = "edit-credential")
+    private WebElement editCredentialButton;
+
+    @FindBy(className = "delete-credential")
+    private WebElement deleteCredentialButton;
+
+    private WebDriver driver;
+
     public HomePage(WebDriver driver) {
+        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
@@ -45,21 +88,28 @@ public class HomePage {
 
     public void navigateToNotesTab() {
         notesTab.click();
-    }
+    };
 
     public void navigateToCredentialsTab() {
         credentialsTab.click();
     }
 
-    public void showNoteModal() {
+    public void showNotes() {
+        WebElement tab = driver.findElement(By.xpath("//a[@href='#nav-notes']"));
+
+        tab.click();
+    }
+
+    public void showNotesModal() {
         showNoteModalButton.click();
     }
 
-    public void createNote(String title, String description) {
-
+    public void createNote(String title, String description) throws InterruptedException {
+        Thread.sleep(500);
         noteTitleInput.sendKeys(title);
-        noteTitleInput.sendKeys(description);
-        noteSubmitButton.click();
+        noteDescriptionInput.sendKeys(description);
+
+        noteSubmitButton.submit();
     }
 
     public String getFirstNoteDescription() {
@@ -67,6 +117,70 @@ public class HomePage {
     }
 
     public String geFirstNoteTitle() {
-        return this.firstNoteTitle.getText();
+        return firstNoteTitle.getText();
+    }
+
+    public String getFirstCredentialUrl() {
+        return firstCredentialUrl.getText();
+    }
+
+    public String getFirstCredentialUsername() {
+        return firstCredentialUsername.getText();
+    }
+
+    public String getFirstCredentialEncryptedPassword() {
+        return firstCredentialEncryptedPassword.getText();
+    }
+
+    public void editNote(String title, String description) throws InterruptedException {
+        Thread.sleep(500);
+
+        editNoteButton.click();
+
+        noteTitleInput.clear();
+        noteDescriptionInput.clear();
+
+        noteTitleInput.sendKeys(title);
+        noteDescriptionInput.sendKeys(description);
+
+        noteSubmitButton.submit();
+    }
+
+    public void deleteNote() {
+        deleteNoteButton.submit();
+    }
+
+    public void showNewCredentialModal() {
+        showCredentialModalButton.click();
+    }
+
+    public void addCredential(String url, String username, String password) throws InterruptedException {
+        Thread.sleep(500);
+
+        credentialUrlInput.sendKeys(url);
+        credentialUsernameInput.sendKeys(username);
+        credentialPasswordInput.sendKeys(password);
+        submitCredentialButton.submit();
+    }
+
+    public void showCredentialModal() {
+        editCredentialButton.click();
+    }
+
+    public void editCredentialUsername(String username) throws InterruptedException {
+        Thread.sleep(500);
+
+        credentialUsernameInput.clear();
+
+        credentialUsernameInput.sendKeys(username);
+        submitCredentialButton.submit();
+    }
+
+    public String getUnencryptedPassword() {
+        return credentialPasswordInput.getAttribute("value");
+    }
+
+    public void deleteCredential() {
+        deleteCredentialButton.submit();
     }
 }
